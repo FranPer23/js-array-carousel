6e5cda10bca3f53d74aaaaa1a0d885f63e913b36
FranPer23 <francescaprt@yahoo.com>
2023-03-14 17:20:25 +0100

    inserimento img

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-const imgArray = ["img/img_1.jpg","img/img_2.jpg","img/img_3.jpg","img/img_4.jpg", "img/img_5.jpg"]
-console.log(imgArray);
+const imgsArray = ["img/img_1.jpg", "img/img_2.jpg", "img/img_3.jpg", "img/img_4.jpg", "img/img_5.jpg"]
 
+console.log(imgsArray);
+
+const imgContainer = document.querySelector(".card")
+
+for (let i = 0; i < imgsArray.length; i++) {
+    const currentImg = imgsArray [i];
+    
+    const objCard = `
+        <div class = "picture">
+            <img src="${currentImg}" alt="">
+        </div>`;
+
+        imgContainer.innerHTML += objCard;
+
+}
+
+const picturesArray = document.getElementsByClassName("picture")
+console.log(picturesArray);
+
+let activeItemIndex = 0;
+picturesArray[activeItemIndex].classList.add("active");
